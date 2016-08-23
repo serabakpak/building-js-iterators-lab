e8362698040374cb1d67b58be9ca31fed0f1510e
Sera Bak <serabak15@gmail.com>
2016-08-22 22:15:15 -0700

    creates myMap function in myMap.js

--- index.js
+++ index.js
@@ -33,9 +33,12 @@ console.log('Expected:', 1000, 'Got:', output);
 /* myEach */
 
 //
-/*myEach(numArray, function print(element, index, arr) {
+myEach(numArray, function print(element, index, arr) {
    console.log('inside myEach', element, index, arr);
- });*/
+ });
+
+
+//my own myEach test driver code
 var array = [];
 var input = numArray;
 myEach(input, function double (num) {
@@ -52,13 +55,16 @@ console.log('Expected:', [0,2,20,200,2000], 'Got:', array);
 
 /* myMap */
 
-/*var input = ["a","b","c"];
+var input = ["a","b","c"];
 var output = myMap(input, function capitalize(v){
     return v.toUpperCase();
 });
 console.log('Testing myMap');
 console.log(output);
 console.log(output[0] === "A" && output[1] === "B" && output[2] === "C"); // assertion
-*/
+
+
+
+
 
 console.log("the end of index.js");
--- starter-code/myMap.js
+++ starter-code/myMap.js
@@ -1,8 +1,11 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Map
 
 function myMap(arr, callback) {
-
-//  CODE INSIDE HERE   //
+	var newArray = [];
+	for (var i = 0; i < arr.length; i++) {
+		newArray.push(callback(arr[i], i, arr));		
+	}
+	return newArray;
 
 }
 
